6aac44ba516ad737067d656a4665fe561b2101af
mathilde renaud <renaud.mathilde95@hotmail.fr>
2024-11-18 15:35:54 +0100

    correct

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_18_142234) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_18_143427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_142234) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -63,4 +65,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_142234) do
   add_foreign_key "bookings", "items"
   add_foreign_key "bookings", "reviews"
   add_foreign_key "bookings", "users"
+  add_foreign_key "items", "users"
 end
