6017ccf749262b5e90643d191935b336a863ed3c
Samylle Jensen <jensensamyllelove@gmail.com>
2024-11-20 12:12:21 +0100

    finished authentication

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "users/sign_up", to: "user#sign_up"
+  get "users/sign_in", to: "user#sign_in"
   # Defines the root path route ("/")
   # root "posts#index"
 end
