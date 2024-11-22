2d8d20c4eeeac75200bc38e18a54e990100ee61c
mathilde renaud <renaud.mathilde95@hotmail.fr>
2024-11-22 11:04:09 +0100

    total price

--- app/javascript/controllers/price_calculator_controller.js
+++ app/javascript/controllers/price_calculator_controller.js
@@ -3,10 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="price-calulator"
 export default class extends Controller {
   static values = {
-    input: String
+    price: Number
   }
 
-  static targets = ["input"]
+  static targets = ["input", "total", "days"]
+
+  connect() {
+    console.log(this.priceValue)
+  }
 
   count(){
     const dates = this.inputTarget.value;
@@ -14,23 +18,23 @@ export default class extends Controller {
     const start_range = date[0];
     const end_range = date[2];
 
-
     const start = start_range.split('-');
     const end_date = end_range.split('-');
 
     const mdy_start = start[1] + ' '+ start[0] +' '+ start[2];
     const mdy_end =  end_date[1] + ' '+ end_date[0] +' '+ end_date[2];
-    console.log(mdy_start);
-    console.log(mdy_end);
-
 
     const start_date = Date.parse(mdy_start);
     const final_date = Date.parse(mdy_end);
-    console.log(start_date);
-    console.log(final_date);
 
     const difference = final_date - start_date
     console.log(difference)
+    const number_of_days = difference/1000/60/60/24 +1
+
+    const total = number_of_days* this.priceValue
+    console.log(total)
+    this.totalTarget.innerText = total
+    this.daysTarget.innerText = number_of_days
 
   }
 }
